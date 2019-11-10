ecd62b8b4462b1c1584a3ee827f56255d5ae913c
Felipe <felipe.dav.alf@gmail.com>
2019-11-10 16:22:36 -0600

    Agregado enemigos aleatorios

---
+++ Assets/script/enemigo_random.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemigo_random : MonoBehaviour{
+    public GameObject enemigo;
+    void Start(){
+        for (int i = 0; i < 20; i++){
+            Vector3 random_position= new Vector3(Random.Range(28,37),1,Random.Range(20,30));
+            Instantiate(enemigo,random_position, this.transform.rotation);
+        }
+        
+            
+    }
+}
