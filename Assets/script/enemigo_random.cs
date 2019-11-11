5c8c531e1bf310c838bcff1fb6163fba62d5af68
Felipe <felipe.dav.alf@gmail.com>
2019-11-10 22:13:53 -0600

    Enemigos siguen al jugador

--- Assets/script/enemigo_random.cs
+++ Assets/script/enemigo_random.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class enemigo_random : MonoBehaviour{
     public GameObject enemigo;
     void Start(){
-        for (int i = 0; i < 20; i++){
-            Vector3 random_position= new Vector3(Random.Range(28,37),1,Random.Range(20,30));
-            Instantiate(enemigo,random_position, this.transform.rotation);
-        }
+        StartCoroutine(Ejemplo());
         
-            
+    }
+    //Vector3 random_position= new Vector3(Random.Range(27.5,37),1,Random.Range(20,30));
+    IEnumerator Ejemplo(){
+        for (int i = 0; i < 50; i++){
+            Vector3 random_position= new Vector3(Random.Range(0,2)==0?Random.Range(27.5f,30f):Random.Range(34.5f,37f),0.5f,Random.Range(20,30));
+            Instantiate(enemigo,random_position, this.transform.rotation);
+            yield return new WaitForSeconds(1);
+        } 
+
     }
 }
---
+++ Assets/script/seguir.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seguir : MonoBehaviour
+{
+     GameObject tanque;
+    public float speed=1;
+    // Update is called once per frame
+    void Start(){
+        tanque=GameObject.FindGameObjectWithTag("tanque");
+    }
+    void Update(){
+        //float distancia=Vector3.Distance(tanque.transform.position,transform.position);
+        transform.position=Vector3.MoveTowards(transform.position,tanque.transform.position,speed*Time.deltaTime);
+    }
+}
