62a41ee164d80e914278c959ccaca1c3fc3dbbf9
Felipe <felipe.dav.alf@gmail.com>
2019-11-14 11:50:33 -0600

    Menu y seleccion de armas vacio

--- Assets/script/seguir.cs
+++ Assets/script/seguir.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class seguir : MonoBehaviour
 {
-     GameObject tanque;
+    GameObject tanque;
+    static int count=50;
+    public TextMesh vida;
     public float speed=1;
     // Update is called once per frame
     void Start(){
@@ -14,4 +17,15 @@ public class seguir : MonoBehaviour
         //float distancia=Vector3.Distance(tanque.transform.position,transform.position);
         transform.position=Vector3.MoveTowards(transform.position,tanque.transform.position,speed*Time.deltaTime);
     }
+    void OnCollisionEnter(Collision other){
+            if(other.gameObject.name=="tanque"){
+                count--;
+                vida.text="Vida:";
+                vida.text=vida.text+count;
+                Debug.Log(vida.text);
+                if(count==0){
+                     Destroy(tanque, 0);
+                }
+            }
+    }      
 }
